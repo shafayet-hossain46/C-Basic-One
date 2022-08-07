e77314f0b22463566ec390dfa7399ca1220ce6a0
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-08-07 14:07:14 +0600

    Learned how make a new line with the backward slash

--- helloWorld.c
+++ helloWorld.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main(){
-    printf("Hello World");
+    printf("Hello World\nIt's me");
     return 0;
 }
---
+++ prac.js
@@ -0,0 +1,19 @@
+// const arr = ["Ami", "Rup",  "Suchishmeeta", "Praptika"]
+// let longestOne = "";
+// for(let i = 0; i < arr.length; i++){
+//     const element = arr[i]
+//     if(element.length > longestOne.length){
+//         longestOne = element;
+//     }
+// }
+// console.log(longestOne);
+const arr = ["Amisafsdfsdafsadffd", "Rup", "Suchishmeeta", " Praptika"]
+let leng = 0;
+let longest = "";
+for (let i = 0; i < arr.length; i++) {
+    if (leng < arr[i].length) {
+        longest = arr[i];
+        leng = arr[i].length;
+    }
+}
+console.log(longest)
